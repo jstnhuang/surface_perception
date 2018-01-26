1ee81df1677e448f49374f2ab3ba06beea8a6863
Yu-Tang Peng <pengy25@cs.washington.edu>
2018-01-25 17:00:00 -0800

    Documentation for SurfaceFinder in progress

--- include/surface_perception/surface_finder.h
+++ include/surface_perception/surface_finder.h
@@ -10,10 +10,43 @@
 #include "pcl/ModelCoefficients.h"
 
 namespace surface_perception {
+/// \brief SurfaceFinder attempt to find multiple horizontal surfaces given a shelf scene.
+///
+/// This class is designed to find surfaces in a shelf scene such as a bookshelf. 
+/// In particular, this class finds the horizontal surfaces if the following conditions
+/// are met:
+/// 1. No NaN points in the input cloud
+/// 2. Each target surface has different height
+/// 
+/// If the input cloud meets the requirement. Mutation functions of this class can be used
+/// to adjust the parameters based on the scenario of the point cloud scene.
+///
+/// \b Example usage:
+/// \code
+///   SurfaceFinder finder;
+///   finder.setCloud(pcl_cloud);
+///   finder.setCloudIndices(point_indices);
+///   finder.setToleranceAngle(10);
+///   finder.setMaxPointDistance(0.01);
+///   finder.setMaxIteration(1000);
+///   finder.setSurfacePointThreshold(10000);
+///
+///   std::vector<pcl::PointIndices::Ptr> indices;
+///   std::vector<pcl::ModelCoefficients> coeffs;
+///   finder.exploreSurfaces(0, 10, &indices, &coeffs);
+/// \endcode
 class SurfaceFinder {
  public:
+  /// \brief Default constructor
   SurfaceFinder();
+
+  /// \brief Set the input point cloud
+  ///
+  /// NaN values in the input cloud should be removed before being passed
+  /// to this function.
   void setCloud(const pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud);
+
+  
   void setCloudIndices(const pcl::PointIndices::Ptr indices);
   void setToleranceAngle(const double& degrees);
   void setMaxPointDistance(const double& dist);
