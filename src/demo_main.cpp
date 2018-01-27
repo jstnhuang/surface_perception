0e1019cafc8c4603ed63ebab68bd2a23a4d01215
Yu-Tang Peng <pengy25@cs.washington.edu>
2018-01-26 17:07:59 -0800

    Changed margin above surface value

--- src/demo_main.cpp
+++ src/demo_main.cpp
@@ -56,7 +56,7 @@ void Demo::Callback(const sensor_msgs::PointCloud2ConstPtr& cloud) {
   ros::param::param("horizontal_tolerance_degrees",
                     horizontal_tolerance_degrees, 10.0);
   double margin_above_surface;
-  ros::param::param("margin_above_surface", margin_above_surface, 0.01);
+  ros::param::param("margin_above_surface", margin_above_surface, 0.015);
   double cluster_distance;
   ros::param::param("cluster_distance", cluster_distance, 0.015);
   int min_cluster_size;
