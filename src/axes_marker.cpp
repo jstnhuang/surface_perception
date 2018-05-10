ee4dd2f79744f918a7c2d97b46b87b67fcfe75b1
Yu-Tang Peng <pengy25@cs.washington.edu>
2018-05-10 16:58:45 -0700

    Removed redundant quaternion conversion

--- src/axes_marker.cpp
+++ src/axes_marker.cpp
@@ -95,12 +95,8 @@ visualization_msgs::MarkerArray GetAxesMarkerArray(
   z_axis.scale.x = z_axis.scale.y = std::max(scale * 0.1, 0.01);
   z_axis.scale.z = scale;
 
-  // Z axis orientation, which has the same rotation_matrix from the given pose.
-  Eigen::Quaternionf z_quaternion(rotation_matrix);
-  z_axis.pose.orientation.x = z_quaternion.x();
-  z_axis.pose.orientation.y = z_quaternion.y();
-  z_axis.pose.orientation.z = z_quaternion.z();
-  z_axis.pose.orientation.w = z_quaternion.w();
+  // Z axis orientation, which has the same orientation as the given pose.
+  z_axis.pose.orientation = pose.orientation;
 
   // Z axis position shifting
   z_axis.pose.position = pose.position;
